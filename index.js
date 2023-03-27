bee7720352b8a5c2efd9bde739ae0067c53ab698
harshit <harshit.sahu@gmail.com>
2023-03-28 01:03:02 +0530

    updated

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ theme.addEventListener("change", () => {
     // alert("hii")
     document.body.classList.toggle("light")
 })
+
+let openResume = document.getElementById("resume-button-1");
+    
+    openResume.addEventListener("click", () => {
+
+    window.open('https://drive.google.com/file/d/1o5aVoIEX1jvMv0Rc4FA6M-fdkuSlXnmp/view?usp=share_link', '_blank'); 
+    
+   })
+
+   let openResumetwo = document.getElementById("resume-button-2");
+    
+    openResumetwo.addEventListener("click", () => {
+        
+    window.open('https://drive.google.com/file/d/1o5aVoIEX1jvMv0Rc4FA6M-fdkuSlXnmp/view?usp=share_link', '_blank'); 
+    
+   })
