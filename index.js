a3d3b835bd3baac51472772df25b6ec9d65cb4e8
harshit <harshit.sahu@gmail.com>
2023-05-10 15:00:48 +0530

    updated

--- index.js
+++ index.js
@@ -23,9 +23,8 @@ let openResume = document.getElementById("resume-button-1");
    let openResumetwo = document.getElementById("resume-link-2");
     
     openResumetwo.addEventListener("click", () => {
-        setTimeout(myGreeting, 5000);
-        function myGreeting(){
+       
             window.open('https://drive.google.com/file/d/1o5aVoIEX1jvMv0Rc4FA6M-fdkuSlXnmp/view?usp=share_link', '_blank'); 
     
-        }
+        
    })
