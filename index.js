7c58265d573f7cbac66ecc3834ba7a1d19a25cd0
harshit <harshit.sahu@gmail.com>
2023-05-10 14:51:52 +0530

    updated

--- index.js
+++ index.js
@@ -20,10 +20,12 @@ let openResume = document.getElementById("resume-button-1");
     
    })
 
-   let openResumetwo = document.getElementById("resume-button-2");
+   let openResumetwo = document.getElementById("resume-link-2");
     
     openResumetwo.addEventListener("click", () => {
-        
-    window.open('https://drive.google.com/file/d/1o5aVoIEX1jvMv0Rc4FA6M-fdkuSlXnmp/view?usp=share_link', '_blank'); 
+        setTimeout(myGreeting, 2000);
+        function myGreeting(){
+            window.open('https://drive.google.com/file/d/1o5aVoIEX1jvMv0Rc4FA6M-fdkuSlXnmp/view?usp=share_link', '_blank'); 
     
+        }
    })
